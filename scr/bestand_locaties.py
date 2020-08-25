a8c3ad42df425648097d1a17f7217424e5e4fb4f
Bais <32563996+nilanbais@users.noreply.github.com>
2020-08-25 17:45:56 +0200

    Update bestand_locaties.py
    made cloud-proof

--- scr/bestand_locaties.py
+++ scr/bestand_locaties.py
@@ -5,65 +5,79 @@ zodat deze locaties in de andere scripts geïmporteerd en gebruikt kunnen worden
 import os
 import pandas as pd
 
-# TODO: verander de adressen die worden gebruikt van lokaal naar cloud.
+from find_storage_path import *
+import getpass
+
+find_storage_path()
+
 # Definiëren van de mappen en documenten
-# Pad naar de algemene TBI One Drive Map (tijdenlijke centrale opslag)
-TBI_Holding_OneDrive = 'C:\\Users\\NBais\\TBI Holding'
+while True:
+    if os.getcwd().endswith('DocumentenLobby'):
+        backend = os.getcwd()
+        break
+    else:
+        os.chdir('..')
 
-# Pad voor de front-end (opslag van FMECAs, RAMS etc.)
-# en de back-en (opslag main script, referentiebestanden en back-ups)
-Front_End_Opslag = os.path.join(TBI_Holding_OneDrive, 'Document Management Systeem - General')
-Back_End_Opslag = os.path.join(TBI_Holding_OneDrive, 'Document Management Systeem - Back-end - Back-end')
+res_path = os.path.abspath('res')
 
-# Specificatie front-end mappen
-Projecten_map = os.path.join(Front_End_Opslag, 'Centrale opslag')
+os.chdir(res_path)
+with open('centrale_opslag_path.txt', 'r') as file:
+    text = file.read()
+file.close()
+
+centrale_opslag = text
+
+# Front end Opslag (MS_teams map
+user_path = os.path.join('C:\\Users', getpass.getuser())
+tbi_path = os.path.join(user_path, 'TBI Holding')
+Front_End_Opslag = os.path.join(tbi_path, 'Document Management Systeem - General')
 
 # Specificatie back-end mappen/bestanden
-Map_Referentie_Documenten = os.path.join(Back_End_Opslag, 'Referentie documenten')
+sbs_path = os.path.join(res_path, 'sbs')
 
 # Locatie en inlezen van de generieke SBS
-Overzicht_SBS_Num_Generiek = os.path.join(Map_Referentie_Documenten, 'Overzicht_SBS_num_Generiek.csv')
+Overzicht_SBS_Num_Generiek = os.path.join(sbs_path, 'Overzicht_SBS_num_Generiek.csv')
 SBS_Generiek = pd.read_csv(Overzicht_SBS_Num_Generiek, sep=';')
 
 # Locatie en inlezen van de referentie SBS van de Coentunnel
-Overzicht_SBS_Num_Coentunnel = os.path.join(Map_Referentie_Documenten, 'Overzicht_SBS_num_Coentunnel.csv')
+Overzicht_SBS_Num_Coentunnel = os.path.join(sbs_path, 'Overzicht_SBS_num_Coentunnel.csv')
 SBS_Coentunnel = pd.read_csv(Overzicht_SBS_Num_Coentunnel, sep=';')
 
 # Locatie en inlezen van de referentie SBS van de Maastunnel
-Overzicht_SBS_Num_Maastunnel = os.path.join(Map_Referentie_Documenten, 'Overzicht_SBS_num_Maastunnel.csv')
+Overzicht_SBS_Num_Maastunnel = os.path.join(sbs_path, 'Overzicht_SBS_num_Maastunnel.csv')
 SBS_Maastunnel = pd.read_csv(Overzicht_SBS_Num_Maastunnel, sep=';')
 
 # Locatie en inlezen van de referentie SBS van MaVa
-Overzicht_SBS_Num_MaVa = os.path.join(Map_Referentie_Documenten, 'Overzicht_SBS_num_MaVa.csv')
+Overzicht_SBS_Num_MaVa = os.path.join(sbs_path, 'Overzicht_SBS_num_MaVa.csv')
 SBS_MaVa = pd.read_csv(Overzicht_SBS_Num_MaVa, sep=';')
 
 # Locatie en inlezen van de referentie SBS van de Westerscheldetunnel
-Overzicht_SBS_Num_Westerscheldetunnel = os.path.join(Map_Referentie_Documenten,
+Overzicht_SBS_Num_Westerscheldetunnel = os.path.join(sbs_path,
                                                      'Overzicht_SBS_num_Westerscheldetunnel.csv')
 SBS_Westerscheldetunnel = pd.read_csv(Overzicht_SBS_Num_Westerscheldetunnel, sep=';')
 # Locatie en inlezen van de RAMS specifieke SBS van de Westerscheldetunnel
-Overzicht_SBS_Num_Westerscheldetunnel_RAMS = os.path.join(Map_Referentie_Documenten,
+Overzicht_SBS_Num_Westerscheldetunnel_RAMS = os.path.join(sbs_path,
                                                           'Overzicht_SBS_num_Westerscheldetunnel_RAMS.csv')
 SBS_Westerscheldetunnel_RAMS = pd.read_csv(Overzicht_SBS_Num_Westerscheldetunnel_RAMS, sep=';')
 
 # Locatie en inlezen van de referentie SBS van de Rijnlandroute
-Overzicht_SBS_Num_Rijnlandroute = os.path.join(Map_Referentie_Documenten, 'Overzicht_SBS_num_Rijnlandroute.csv')
+Overzicht_SBS_Num_Rijnlandroute = os.path.join(sbs_path, 'Overzicht_SBS_num_Rijnlandroute.csv')
 SBS_Rijnlandroute = pd.read_csv(Overzicht_SBS_Num_Rijnlandroute, sep=';')
 
 # Locatie en inlezen van de referentie van de document typen en klassen
-Overzicht_Document_Klasse_Type = os.path.join(Map_Referentie_Documenten, 'Overzicht_Document_Klasse_Type.csv')
+Overzicht_Document_Klasse_Type = os.path.join(res_path, 'Overzicht_Document_Klasse_Type.csv')
 Document_Klasse_Type = pd.read_csv(Overzicht_Document_Klasse_Type, sep=';')
 
 # Locatie en inlezen van de referentie van het eigenaarschap
-Overzicht_Eigenaarschap_Documenten = os.path.join(Map_Referentie_Documenten, 'Overzicht_Eigenaarschap_documenten.csv')
+Overzicht_Eigenaarschap_Documenten = os.path.join(res_path, 'Overzicht_Eigenaarschap_documenten.csv')
 Referentietabel_Eigenaarschap = pd.read_csv(Overzicht_Eigenaarschap_Documenten, sep=';', encoding='latin1')
 
 # Locatie en inlezen van het overzicht van de personen en de persoonsinformatie
-Overzicht_Personen_Informatie = os.path.join(Map_Referentie_Documenten, 'Overzicht_Personen_Informatie.csv')
+Overzicht_Personen_Informatie = os.path.join(res_path, 'Overzicht_Personen_Informatie.csv')
 Personen_Informatie = pd.read_csv(Overzicht_Personen_Informatie, sep=';')
 
 # Locatie en inlezen van het overzicht van de status aanduidingen
-Overzicht_Status_Aanduiding = os.path.join(Map_Referentie_Documenten, 'Overzicht_Status_aanduiding.csv')
+Overzicht_Status_Aanduiding = os.path.join(res_path, 'Overzicht_Status_aanduiding.csv')
 Status_Aanduiding = pd.read_csv(Overzicht_Status_Aanduiding, sep=';')
 
 # Standaard deel export url voor het makan van links naar de documenten
