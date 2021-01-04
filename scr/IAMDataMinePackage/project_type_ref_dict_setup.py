411698133fd80330beca6dc61aa6746a3d78d62d
Nilan <nilan.bais@croonwolterendros.nl>
2021-01-04 15:15:15 +0100

    Update project_type_ref_dict_setup.py

--- scr/IAMDataMinePackage/project_type_ref_dict_setup.py
+++ scr/IAMDataMinePackage/project_type_ref_dict_setup.py
@@ -36,15 +36,116 @@ def sort_objects_per_project():
     return objecten_per_project
 
 
-# Uitvoeren van de gedefininieerde functie
-dict_objecten_per_project = sort_objects_per_project()
+def get_kunstwerk_type(object):
+    """
+    Deze functie neemt het object als input en geeft het type van dit object(kunstwerk) als output.
+    :param object: De omschrijving van het object (sluis, tunnel, etc.)
+    :return: Het type van het kunstwerk dat als input is gegeven.
+    """
+    # Inlezen van het referentiedocument
+    # todo: deze kan misschien beter omgevormd worden naar global
+    overzicht_objecten_typen = Definities_Object_Typen
 
-# lijst projecten
-p = list(dict_objecten_per_project.keys())
+    # Isoleren van de informatie m.b.t. het object
+    try:
+        object_info = overzicht_objecten_typen.loc[overzicht_objecten_typen['object_type'] == object.capitalize()]
 
-# Inlezen van de referentie documenten
-definitie_object_type = Definities_Object_Typen
+    except AttributeError:
+        """Wordt gegeven als het eerste teken van de string een cijfer is."""
+        object_info = overzicht_objecten_typen.loc[overzicht_objecten_typen['object_type'] == object]
 
-# Dict initialiseren om op te vullen
-type_object_per_project = dict()
+    # Output waarde gereedstellen
+    try:
+        output = object_info['kunstwerk_type'].iloc[0]
+        return output
 
+    except IndexError as e:
+        """Wordt gegeven als de object_type van een object niet is ingevuld (nan)"""
+        print(f'IndexError: \'{e}\' for object: {object}\nOutput type set as: n.v.t.')
+        error_output = 'n.v.t.'
+        return error_output
+
+
+def change_objects_to_types(overzicht_objecten_per_project):
+    """
+    Deze functie neemt het overzicht(dtype: dict) van de objecten per project als input en geeft een dict terug
+    waarvan de objecten per project zijn vervangen door de kunstwerk typen van de objecten van de projecten.
+    :param overzicht_objecten_per_project: Overzicht van de verschillende objecten per project (dtype: dict)
+    :return: Dict met de kunstwerk typen per project.
+    """
+    # lijst projecten
+    projecten = list(overzicht_objecten_per_project.keys())
+
+    # Dict initialiseren om op te vullen
+    dict_type_object_per_project = dict()
+
+    for _ in projecten:
+
+        # lijst met object soorten per project
+        lijst_objecten_per_project = overzicht_objecten_per_project[_]
+
+        # lijst met de type kunstwerken per project
+        lijst_kunstwerk_typen_per_project = [get_kunstwerk_type(a) for a in lijst_objecten_per_project]
+
+        # key: val pair in nieuwe dict in de vorm project: type_kunstwerken
+        dict_type_object_per_project[_] = lijst_kunstwerk_typen_per_project
+
+    return dict_type_object_per_project
+
+
+def type_per_project(overzicht_type_object_per_project):
+    """
+    Functie voor het samenvatten van de typen kunstwerken in 1 uitdrukking (nat/droog/combinatie) per project.
+    :param overzicht_type_object_per_project: Het overzicht met de verschillende objecten per project (dtype: dict)
+    :return: Dict met een sammenvattende uitdrukking van het type van het project in de vorm [project: type].
+    """
+    # lege dict om op te vullen
+    project_types = dict()
+
+    # lijst projecten
+    projecten = list(overzicht_type_object_per_project.keys())
+
+    # itereren over de verschillende projecten
+    for project in projecten:
+        # Kijken hoe vaak de verschillende waarden voorkomen
+        count_of_object_types = list(Counter(overzicht_type_object_per_project[project]))
+
+        # als meer dan 1, dan zijn er meer typen per project
+        # todo: elif schijven voor (len > 1 en n.v.t. in count_of_object_types)
+        if len(count_of_object_types) > 1 and ('n.v.t.' not in count_of_object_types):
+            project_type = "Combinatie"
+        else:
+            project_type = count_of_object_types[0]  # verkrijgen van de
+
+        # project en bijbehorende types toevoegen aan dict
+        project_types[project] = project_type
+
+    return project_types
+
+
+def get_types_summary():
+    """
+    Deze statische functie voegt alle lokale functies samen in één functie. Zo kan het overzicht van de projecten
+    met daarbij hun type met één statement verkregen worden.
+    :return: Dict met een sammenvattende uitdrukking van het type van het project in de vorm [project: type].
+    """
+    # Uitvoeren van de gedefininieerde functie
+    dict_objecten_per_project = sort_objects_per_project()
+
+    # lijst met verschillende kunstwerk typen per project
+    lijst = change_objects_to_types(dict_objecten_per_project)
+
+    # De typen bekeken per project
+    typen_per_project = type_per_project(lijst)
+
+    return typen_per_project
+
+
+if __name__ == '__main__':
+    import time
+    start = time.time()
+
+    summary = get_types_summary()
+
+    end = time.time()
+    print(f'runtime {end-start} sec')
