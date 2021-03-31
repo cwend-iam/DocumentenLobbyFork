0db66059a17bfed64dab42120aa18064f197355f
Nilan <nilan.bais@croonwolterendros.nl>
2021-03-31 16:55:19 +0200

    Update test_new_di_module.py

--- test/Test_env/test_new_di_module.py
+++ test/Test_env/test_new_di_module.py
@@ -19,28 +19,24 @@ while True:
 
 # folder with docs in test_env
 test_docs_folder = 'test/Test_env/doc'
-file_name = 'Faalkansanalyse Di 25 en Di 26 - Evacuatie- en Verkeersgeleidingsverlichting Versie A.pdf'
+file = 'E601.072 - Beveiliging en bewaking - 2001-1194'
 
 sbs_overview = pd.read_excel('res/sbs' + '/' + 'sbs_koppeldoc_dl.xlsx')
-projectnaam = 'Coentunnel-tracé'
+project = 'Rijnlandroute'
+
+lijst_projecten = os.listdir(bestand_locaties.centrale_opslag)
 
 """
-onderstaande code werkt voor het herkennen van di_nummers als 10, 22, en 46A (uit testset documenten in Test_env)
-loopt spaak op documenten die meerdere di_nums in titel hebben (25 en 26).
-De koppeling tussen found_project_numbers en de bijbehorende generieke nummers moet gemaakt worden. Hiervoor moet
-waarschijnlijk per found_project_number gekeken worden of deze aanwezig is in de sbs_overview.
-Kijk of er iets geregeld kan worden met behulp van any() (https://www.geeksforgeeks.org/python-check-if-any-list-element-is-present-in-tuple/)
-waarschijnlijk moet er gekeken worden of een gevonden nummer aanwezig is in de project kolom ipv anders om (zoals nu nog het geval is)
-Misschien niet, maar het is misschien nog goed om te kijken naar het verschil tussen de eigenschappen van een 
-tuple en een set. het kan zijn dat een set eigenschappen heeft die deze aanpak wel werkbaar maakt.
-https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s07.html
+De workflow van de module is gebouwd op de faalkananalysen van de Countunnel. De workflow moet nog worden getest voor
+de andere projecten.
 """
 ## Module body
 
 
 def string_to_tuple(string):
     """
-    Transforms a tuple that was formed to a string in the extraction back to a tuple for further handling.
+    Transforms a tuple that was formed to a string in the extraction-phase back to a tuple for further
+    handling. "(25, 61)" => ("25", "61")
     :param string:
     :return:
     """
@@ -53,39 +49,121 @@ def string_to_tuple(string):
     return values
 
 
-project_column = f'sbs nummer {projectnaam}'
-known_project_numbers = list(sbs_overview[project_column])
-known_project_numbers = [x for x in known_project_numbers if not isinstance(x, float)]
+def contains_any(string, set_var):
+    """
+    Check whether sequence str contains ANY of the items in set.
+    Function found at source: https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s07.html
+    """
+    return 1 in [c in string for c in set_var]
+
 
 """
-found_project_numbers contains all the project specific sbs numbers that are found in the file name.
+kijk voor onderstaande functie of de map functie iets kan betekeken. ook kijken of het nuttig is te kijken naar
+de lengte van de gevonden_sbs_nummers (qua aanpak voor het controleren van de nummers.
+zie info: https://www.oreilly.com/library/view/python-cookbook/0596001673/ch03s07.html
 """
-found_project_numbers = []
-for n in known_project_numbers:
-    if str(n) in file_name:
-        found_project_numbers.append(n)
-    elif isinstance(n, str) and ',' in n:
-        split_numbers = string_to_tuple(n)
-        num2append = [num for num in split_numbers if num in file_name]
-        if len(num2append) != 0:
-            for num in num2append:
-                found_project_numbers.append(num)
-    else:
-        pass
+def check_numbers(project_sbs_number, found_sbs_numbers):
+    """
+    function to check whether a project specific number is in the collection of found numbers from the file title
+    :param project_sbs_number:
+    :param found_sbs_numbers:
+    :return:
+    """
+    # Making set of the found numbers
+    found_sbs_numbers_set = set(found_sbs_numbers)
+    """
+    The if statement bellow covers the case of multiple project specific numbers pointing to one generic sbs number.
+    If any of the project specific numbers is found in the 'found numbers set', return True. If none is found,
+    return False.
+    """
+    if ',' in project_sbs_number:
+        project_sbs_nummer_set = string_to_tuple(project_sbs_number)
+        return 1 in [c in found_sbs_numbers_set for c in project_sbs_nummer_set]
+    else:  # check if project_sbs_number is in found_sbs_numbers_set
+        return 1 if project_sbs_number in found_sbs_numbers_set else 0
 
-# Transforming data types of values to tuples in the sbs_overview to support the check to gather the result_list
-for row_index, row in sbs_overview.iterrows():
-    for col_index, value in row.items():
-        if (col_index != 'sbs omschrijving') and (isinstance(value, str)) and (',' in value):
-            new_value = string_to_tuple(value)
-            sbs_overview[col_index][row_index] = new_value
+
+def di_number_v2(file_name, projectnaam):
+
+    project_column = f'sbs nummer {projectnaam}'
+    known_project_numbers = list(sbs_overview[project_column])
+    known_project_numbers = [x for x in known_project_numbers if not isinstance(x, float)]
+
+    """
+    found_project_numbers contains all the project specific sbs numbers that are found in the file name.
+    """
+    found_project_numbers = []
+    for n in known_project_numbers:
+        if str(n) in file_name:
+            found_project_numbers.append(str(n))
+            """
+            n is added as a string to make it possible to apply the function 'contains_any()' in the list 
+            comprehension for result_list.
+            """
+        elif isinstance(n, str) and ',' in n:
+            split_numbers = string_to_tuple(n)
+            num2append = [num for num in split_numbers if num in file_name]
+            if len(num2append) != 0:
+                for num in num2append:
+                    found_project_numbers.append(num)
+        else:
+            pass
+
+    # Filling the nan values to make the list comprehension for result_list possible
+    sbs = sbs_overview.fillna(value=-1)
+
+    gen_col = 'sbs nummer generiek'
+    # result_list = [sbs_overview.at[index, gen_col] for index in range(sbs_overview.shape[0])
+    #                if contains_any(str(sbs_overview.at[index, project_column]), found_project_numbers)]
+    """
+    De list comprehension voor de result_list in nog niet geheel correct. De IF statement van de comprehension 
+    geeft ook een hit wanneer '0' (project_specefiek) met '30' (generiek) wordt vergeleken. kijk of dit met behulp
+    van een statische functie is op te lossen (functie enkel verantwoordelijk voor de check tussen de verschillende
+    project_speciefieke cijfers met het desbetreffende generieke nummer. if True => add to result_list.
+    """
+    result_list = [sbs.at[index, gen_col] for index in range(sbs.shape[0])
+                   if check_numbers(project_sbs_number=str(sbs.at[index, project_column]),
+                                    found_sbs_numbers=found_project_numbers)]
+
+    # Return statement (print statement for now)
+    return result_list
 
 
-gen_col = 'sbs nummer generiek'
-result_list = [sbs_overview.at[index, gen_col]
-               for index in range(sbs_overview.shape[0])
-               if sbs_overview.at[index, project_column] in found_project_numbers]
+num = di_number_v2(file_name=file, projectnaam=project)
+print(f'di num = {num}')
 
-print(sbs_overview.at[58, project_column])
 
-## Return statement (print statement for now)
+def di_name_v2(di_num):
+    if di_num == (9009 or 9999):
+        di_name = 'n.v.t.'
+        return di_name
+
+    di_num = set(list(di_num))
+    sbs = sbs_overview
+    gen_col = 'sbs nummer generiek'
+    name_col = 'sbs omschrijving'
+    di_name = [sbs.at[index, name_col] for index in range(sbs.shape[0]) if sbs.at[index, gen_col] in di_num]
+    return di_name
+
+
+print(di_name_v2(di_num=num))
+
+
+def discipline_v2(di_num):
+    if di_num == (9009 or 9999):
+        di_name = 'n.v.t.'
+        return di_name
+
+    di_num = set(di_num)
+    sbs = sbs_overview
+    gen_col = 'sbs nummer generiek'
+    discipline_col = 'discipline'
+    discipline = [sbs.at[index, discipline_col] for index in range(sbs.shape[0]) if sbs.at[index, gen_col] in di_num]
+
+    unique = set(discipline)
+    print(f'unique = {unique}')
+
+    return unique
+
+
+print(discipline_v2(di_num=num))
